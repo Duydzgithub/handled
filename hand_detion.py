fe380ee59f3dfb5b6dca813c61df84cb5dc81ae2
Duydzgithub <139465504+Duydzgithub@users.noreply.github.com>
2024-08-01 21:47:21 +0700

    hand_detion.py

--- hand_detion.py
+++ hand_detion.py
@@ -1,37 +1,43 @@
 import cv2
-import controller as cnt
 from cvzone.HandTrackingModule import HandDetector
+from controller import cnt 
+import pyfirmata
+# # Nhập cổng COM kết nối với Arduino ở đây
+comport='COM3'
 
-detector=HandDetector(detectionCon=0.8,maxHands=1)
+board=pyfirmata.Arduino(comport)
 
-video=cv2.VideoCapture(0)
+# Nối 5 led với các chân từ D2 tới D6
+led_1=board.get_pin('d:2:o')
+led_2=board.get_pin('d:3:o')
+led_3=board.get_pin('d:4:o')
+led_4=board.get_pin('d:5:o')
+led_5=board.get_pin('d:6:o')
+led_l = [led_1, led_2, led_3, led_4, led_5]
+def led(fingerUp):
+    fc = 0
+    for x in range(len(fingerUp)):
+        led_l[x].write(fingerUp[x])
+        fc += fingerUp[x]
+    cv2.putText(frame, f'Finger count:{fc}', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
+detetor = HandDetector(detectionCon=0.8, maxHands=1)
+
+video = cv2.VideoCapture(0)
 while True:
-    ret,frame=video.read()
-    frame=cv2.flip(frame,1)
-    hands,img=detector.findHands(frame)
+    ret, frame = video.read()
+    frame = cv2.flip(frame,1)
+    hands, img = detetor.findHands(frame)
     if hands:
-        lmList=hands[0]
-        fingerUp=detector.fingersUp(lmList)
-
+        lmlist=hands[0]
+        fingerUp = detetor.fingersUp(lmlist)
         print(fingerUp)
-        cnt.led(fingerUp)
-        if fingerUp==[0,0,0,0,0]:
-            cv2.putText(frame,'Finger count:0',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,0,0,0]:
-            cv2.putText(frame,'Finger count:1',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)    
-        elif fingerUp==[0,1,1,0,0]:
-            cv2.putText(frame,'Finger count:2',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,1,1,0]:
-            cv2.putText(frame,'Finger count:3',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,1,1,1]:
-            cv2.putText(frame,'Finger count:4',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[1,1,1,1,1]:
-            cv2.putText(frame,'Finger count:5',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA) 
+        cnt.led(fingerUp)  # Gọi phương thức led từ đối tượng cnt
+        led(fingerUp)
 
-    cv2.imshow("frame",frame)
-    k=cv2.waitKey(1)
-    if k==ord("k"):
+    cv2.imshow("Window", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
         break
 
 video.release()
